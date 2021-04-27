9c603cd3ec0f308e71b3b659f8ae6500c7e2cd12
HOTGU <56953750+HOTGU@users.noreply.github.com>
2021-04-27 11:39:21 +0900

    ㅇ완성

---
+++ src/routes/Auth.js
@@ -0,0 +1,39 @@
+import AuthForm from "components/AuthForm";
+import { authService, firebaseInstance } from "fbase";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faGoogle,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+
+const Auth = () => {
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    provider = new firebaseInstance.auth.GoogleAuthProvider();
+    await authService.signInWithPopup(provider);
+  };
+
+  return (
+    <div className="authContainer">
+      <FontAwesomeIcon
+        icon={faTwitter}
+        color={"#04AAFF"}
+        size="3x"
+        style={{ marginBottom: 30 }}
+      />
+      <AuthForm />
+      <div className="authBtns">
+        <button onClick={onSocialClick} name="google" className="authBtn">
+          Continue with Google <FontAwesomeIcon icon={faGoogle} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
